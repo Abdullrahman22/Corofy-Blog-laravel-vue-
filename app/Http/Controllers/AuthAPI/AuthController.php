304f7e1fcfd6cfa9c31ddb68cc7566f23875719e
abdullrahman22 <abdullrahman.ismael22@gmail.com>
2020-09-15 03:39:35 +0200

    handle errors in laravel passport

--- app/Http/Controllers/AuthAPI/AuthController.php
+++ app/Http/Controllers/AuthAPI/AuthController.php
@@ -15,33 +15,42 @@ class AuthController extends Controller
     public function register(Request $request)
     {
 
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-        if ($validator->fails())
-        {
-            return response(['errors'=>$validator->errors()->all()], 422);
+        $rules = [
+            'name' => 'required|string|max:50',
+            'email' => 'required|string|email|max:55|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ];
+        $validator = Validator::make( $request->all() , $rules  ); 
+        if( $validator -> fails()) {  
+            return response() -> json([
+                "status" => "error",
+                "errors" => $validator->errors()  // return errors validator in array 
+            ], 422 ); 
         }
+
         $request['password'] = Hash::make($request['password']);
         $request['remember_token'] = Str::random(10);
         $user = User::create($request->toArray());
         $token = $user->createToken('Laravel Password Grant Client')->accessToken;
         $response = ['token' => $token];
         return response($response, 200);
+
     }
 
     public function login(Request $request)
     {
 
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-        if ( $validator->fails() )
-            return response(['errors' => $validator->errors()-> all() ], 422);
-        
+        $rules = [
+            'email' => 'required|string|email|max:55',
+            'password' => 'required|min:8',
+        ];
+        $validator = Validator::make( $request->all() , $rules  ); 
+        if( $validator -> fails()) {  
+            return response() -> json([
+                "status" => "error",
+                "errors" => $validator->errors()  // return errors validator in array 
+            ], 422 ); 
+        }
 
         $user = User::where('email', $request->email)->first();
         if ($user) {
