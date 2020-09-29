246158970d6a22194a70ab97a9d5dcb682da0af8
Abdullrahman22 <abdullrahman.ismael22@gmail.com>
2020-09-29 19:31:26 +0200

    add new style for admin login page

--- resources/views/auth/adminLogin.blade.php
+++ resources/views/auth/adminLogin.blade.php
@@ -2,25 +2,30 @@
 
 @section('webContent')
 
-    <form method="POST" action="{{ route('adminLogin') }}">
-        
-        @csrf
-        <div class="form-group">
-            <label for="exampleInputEmail1"> Email address </label>
-            <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Type Enter email..." autocomplete="off">
-            @error('email')
-                <small class="form-text text-danger">{{$message }}</small> <!-- message is var in laravel -->
-            @enderror
+    <div id="auth-admin">
+        <div class="form">
+            <form method="POST" action="{{ route('adminLogin') }}">
+                <h2 class="text-center">Admin Login</h2>
+                <br>
+                @csrf
+                <div class="form-group">
+                    <label for="exampleInputEmail1"> Email address </label>
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Type Admin Email..." autocomplete="off" required>
+                    @error('email')
+                        <small class="form-text text-danger">{{$message }}</small> <!-- message is var in laravel -->
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1"> Password </label>
+                    <input type="password" class="form-control" name="password" placeholder="Type Password..." autocomplete="off" minlength="8" required>
+                    @error('password')
+                        <small class="form-text text-danger">{{$message }}</small> <!-- message is var in laravel -->
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                
+            </form>
         </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1"> Password </label>
-            <input type="password" class="form-control" name="password" placeholder="Type Password..." autocomplete="off" >
-            @error('password')
-                <small class="form-text text-danger">{{$message }}</small> <!-- message is var in laravel -->
-            @enderror
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        
-    </form>
+    </div>
     
 @endsection
